89ad20913933143798d6deb2ee50f04e3f060056
iiegor <iegormundo@hotmail.com>
2015-10-03 01:00:09 +0200

    Set a proper logfile extension

--- config/default.js
+++ config/default.js
@@ -2,5 +2,5 @@ module.exports = {
   'env': 'dev',
   'port': 1243,
   'allow': '<allow-access-from domain=\"*.xat.dev\" to-ports=\"20,21,80,443,1243\"/>',
-  'logfile': 'xat.logs'
+  'logfile': 'xat.log'
 }
